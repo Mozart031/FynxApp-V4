09d62e61888203a31a0b1f7e293a7078156e2e38
Mozart031 <ericksonp032102@gmail.com>
2026-06-19 13:41:56 -0400

    feat: add iOS RevenueCat API key for subscriptions

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
   REVENUECAT_API_KEY: "goog_PHMTtXSvqYkXDxQoyztxCTFgtlt",
-  REVENUECAT_API_KEY_IOS: "appl_tu_clave_ios_aqui",
+  REVENUECAT_API_KEY_IOS: "appl_CZKuGdTrAtGlFdTdFAnZrrxJIWs",
   ENTITLEMENT_ID: "Premium",
 };
